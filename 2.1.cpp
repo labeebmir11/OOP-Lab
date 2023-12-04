140b475e7ee9256f9488cec03d1d499f34f81f8f
labeebmir11 <121848694+labeebmir11@users.noreply.github.com>
2023-12-04 21:16:57 +0530

    OOP lab: 04-12-2023

---
+++ 2.1.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    float g, cf;
+    cout<<"Enter number of gallons"<<endl;
+    cin>>g;
+
+    cf = g / 7.481;
+    cout<<"Equivalent number of cubic feet is: "<<cf<<endl;
+    return 0;
+}
